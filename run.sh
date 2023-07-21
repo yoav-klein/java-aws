66d83a9da07b9dc778ca0c7eb9fd24b2582f5c18
Yoav <yoavklein25@gmail.com>
2023-07-21 14:42:45 +0300

    clean

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ! -f classpath.txt ]; then
-    mvn dependency:build-classpath -Dmdep.outputFile=classpath.txt
-    echo -n ":./target/my-app-1.0-SNAPSHOT.jar" >> classpath.txt
-fi
-
-java -cp $(cat classpath.txt) com.mycompany.app.App
