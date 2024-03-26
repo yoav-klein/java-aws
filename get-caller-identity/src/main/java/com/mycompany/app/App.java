2b40b0fdec419776f4c53f2b4eef92472fec910d
Yoav Klein <yoavklein25@gmail.com>
2024-03-26 16:25:30 +0000

    STS

--- get-caller-identity/src/main/java/com/mycompany/app/App.java
+++ get-caller-identity/src/main/java/com/mycompany/app/App.java
@@ -1,27 +1,38 @@
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
-import com.amazonaws.services.securitytoken.AWSSecurityTokenService;
-import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceClientBuilder;
-import com.amazonaws.services.securitytoken.model.GetCallerIdentityRequest;
-import com.amazonaws.services.securitytoken.model.GetCallerIdentityResult;
+//import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import software.amazon.awssdk.services.sts.StsClient;
+import software.amazon.awssdk.services.sts.model.GetCallerIdentityRequest;
+import software.amazon.awssdk.services.sts.model.GetCallerIdentityResponse;
+import software.amazon.awssdk.regions.Region;
 
-public class HelloWorld {
+//import com.amazonaws.services.sts.model.GetCallerIdentityResult;
+
+public class App {
     public static void main(String[] args) {
         // Create STS client
-        AWSSecurityTokenService stsClient = AWSSecurityTokenServiceClientBuilder.standard()
-                .withCredentials(DefaultAWSCredentialsProviderChain.getInstance())
-                .build();
 
+        String region = System.getenv("AWS_REGION") == null ? "us-east-1" : System.getenv("AWS_REGION");
+        StsClient client = StsClient.builder()
+            .region(Region.of(region))
+            .build();
+        
+        GetCallerIdentityResponse response = client.getCallerIdentity();
         // Get caller identity
-        GetCallerIdentityRequest request = new GetCallerIdentityRequest();
-        GetCallerIdentityResult result = stsClient.getCallerIdentity(request);
+        //GetCallerIdentityRequest request = new GetCallerIdentityRequest();
+        
+        //GetCallerIdentityResult result = stsClient.getCallerIdentity(request);
 
         // Print caller identity
         while(true) {
-            System.out.println("User ID: " + result.getUserId());
-            System.out.println("Account ID: " + result.getAccount());
-            System.out.println("Arn: " + result.getArn());
+            try {
+                System.out.println("User ID: " + response.userId());
+                System.out.println("Account ID: " + response.account());
+                System.out.println("Arn: " + response.arn());
 
-            Thread.sleep(10000);
+                Thread.sleep(10000);
+
+            } catch(InterruptedException e) {
+                System.out.println("Interrupted..");
+            }
         }
     }
 }
